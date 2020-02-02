55d3a98e8c4899182b0c238618a6f55c2b2667e4
Anthony Nwobodo <anthonynwobodo7@gmail.com>
2020-02-02 19:47:23 +0100

    further changies

--- index.py
+++ index.py
@@ -4,37 +4,59 @@ from functions import *
 from compositions import *
 
 
-questions = [name_age, height_weight, occupation_hobby,
-             complextion_race, city_country]
+def prompt():
+    return input('do you want to run again? [y/n]')
 
 
-randomQuestions = random.choice(questions)
+def compose():
 
-if randomQuestions == name_age:
-    name_and_age = name_age()
+    questions = [name_age, height_weight, occupation_hobby,
+                 complextion_race, city_country]
 
-    print(intro + name_and_age[0] + intro_2 +
-          name_and_age[1] + 'years.' + mid + end)
+    randomQuestions = random.choice(questions)
 
-elif randomQuestions == height_weight:
-    height_and_weight = height_weight()
+    if randomQuestions == name_age:
+        name_and_age = name_age()
 
-    print(start_1_1 + height_and_weight[0] + start_1_2 +
-          height_and_weight[1] + mid_1_1 + end_1_1)
+        print(intro + name_and_age[0] + intro_2 +
+              name_and_age[1] + 'years.' + mid + end + '\n')
+        if prompt() == 'y' or 'yes':
+            compose()
+        else:
+            break
 
-elif randomQuestions == occupation_hobby:
-    occupation_and_hobby = occupation_hobby()
+    elif randomQuestions == height_weight:
+        height_and_weight = height_weight()
 
-    print(start_2_1 + occupation_and_hobby[0] +
-          mid_2_1 + occupation_and_hobby[1] + end_2_1)
+        print(start_1_1 + height_and_weight[0] + start_1_2 +
+              height_and_weight[1] + mid_1_1 + end_1_1 + '\n')
+        if prompt() == 'y' or 'yes':
+            compose()
+        else:
+            break
 
-elif randomQuestions == complextion_race:
-    complextion_and_race = complextion_race()
+    elif randomQuestions == occupation_hobby:
+        occupation_and_hobby = occupation_hobby()
 
-    print(start_3_1 + complextion_and_race[0] +
-          mid_3_1 + complextion_and_race[1] + end_3_1)
-else:
-    city_and_country = city_country()
+        print(start_2_1 + occupation_and_hobby[0] +
+              mid_2_1 + occupation_and_hobby[1] + end_2_1 + '\n')
+        if prompt() == 'y' or 'yes':
+            compose()
 
-    print(city_and_country[0] + start_4_1 +
-          mid_4_1 + city_and_country[1] + end_4_1)
+    elif randomQuestions == complextion_race:
+        complextion_and_race = complextion_race()
+
+        print(start_3_1 + complextion_and_race[0] +
+              mid_3_1 + complextion_and_race[1] + end_3_1 + '\n')
+        if prompt() == 'y' or 'yes':
+            compose()
+    else:
+        city_and_country = city_country()
+
+        print(city_and_country[0] + start_4_1 +
+              mid_4_1 + city_and_country[1] + end_4_1 + '\n')
+        if prompt() == 'y' or 'yes':
+            compose()
+
+
+compose()
